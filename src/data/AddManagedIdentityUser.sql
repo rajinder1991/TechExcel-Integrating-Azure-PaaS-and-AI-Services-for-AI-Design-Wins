c09dee253473c682e9fa33fc8fb5bd82cbf32c94
rajinder1991 <83951506+rajinder1991@users.noreply.github.com>
2025-03-14 03:48:09 +0000

    commit 1

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER [id-sg3paabkgfebq] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [id-sg3paabkgfebq];
+ALTER ROLE db_datawriter ADD MEMBER [id-sg3paabkgfebq];
 
